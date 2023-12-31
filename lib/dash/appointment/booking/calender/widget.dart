31975e307c2d57b7bf5218bcb147a157c5857838
Jake Walklate <jakewalklate@protonmail.com>
2023-12-31 20:08:36 +1100

    remix appointment/booking scaffolding

--- lib/dash/appointment/booking/calender/widget.dart
+++ lib/dash/appointment/booking/calender/widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:booking_calendar/booking_calendar.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:calendar_timeline/calendar_timeline.dart';
 
 class BookingCalendarDemoApp extends StatefulWidget {
   const BookingCalendarDemoApp({Key? key}) : super(key: key);
@@ -11,91 +12,135 @@ class BookingCalendarDemoApp extends StatefulWidget {
 
 class _BookingCalendarDemoAppState extends State<BookingCalendarDemoApp> {
   final now = DateTime.now();
-  late BookingService mockBookingService;
 
   @override
   void initState() {
     super.initState();
-    // DateTime.now().startOfDay
-    // DateTime.now().endOfDay
-    mockBookingService = BookingService(
-        serviceName: 'Mock Service',
-        serviceDuration: 30,
-        bookingEnd: DateTime(now.year, now.month, now.day, 18, 0),
-        bookingStart: DateTime(now.year, now.month, now.day, 8, 0));
-  }
-
-  Stream<dynamic>? getBookingStreamMock(
-      {required DateTime end, required DateTime start}) {
-    return Stream.value([]);
-  }
-
-  Future<dynamic> uploadBookingMock(
-      {required BookingService newBooking}) async {
-    await Future.delayed(const Duration(seconds: 1));
-    converted.add(DateTimeRange(
-        start: newBooking.bookingStart, end: newBooking.bookingEnd));
-    print('${newBooking.toJson()} has been uploaded');
-  }
-
-  List<DateTimeRange> converted = [];
-
-  List<DateTimeRange> convertStreamResultMock({required dynamic streamResult}) {
-    ///here you can parse the streamresult and convert to [List<DateTimeRange>]
-    ///take care this is only mock, so if you add today as disabledDays it will still be visible on the first load
-    ///disabledDays will properly work with real data
-    DateTime first = now;
-    DateTime tomorrow = now.add(const Duration(days: 1));
-    DateTime second = now.add(const Duration(minutes: 55));
-    DateTime third = now.subtract(const Duration(minutes: 240));
-    DateTime fourth = now.subtract(const Duration(minutes: 500));
-    converted.add(
-        DateTimeRange(start: first, end: now.add(const Duration(minutes: 30))));
-    converted.add(DateTimeRange(
-        start: second, end: second.add(const Duration(minutes: 23))));
-    converted.add(DateTimeRange(
-        start: third, end: third.add(const Duration(minutes: 15))));
-    converted.add(DateTimeRange(
-        start: fourth, end: fourth.add(const Duration(minutes: 50))));
-
-    //book whole day example
-    converted.add(DateTimeRange(
-        start: DateTime(tomorrow.year, tomorrow.month, tomorrow.day, 5, 0),
-        end: DateTime(tomorrow.year, tomorrow.month, tomorrow.day, 23, 0)));
-    return converted;
-  }
-
-  List<DateTimeRange> generatePauseSlots() {
-    return [
-      DateTimeRange(
-          start: DateTime(now.year, now.month, now.day, 12, 0),
-          end: DateTime(now.year, now.month, now.day, 13, 0))
-    ];
   }
 
   @override
   Widget build(BuildContext context) {
+    const items = [
+      '8:30am',
+      '9am',
+      '9:30am',
+      '10am',
+      '10:30am',
+      '11am',
+      '11:30am',
+      '12pm',
+      '12:30pm',
+      '1pm',
+      '1:30pm',
+      '2pm',
+      '2:30pm',
+      '3pm',
+      '3:30pm',
+      '4pm',
+      '4:30pm',
+      '5pm',
+      '5:30pm'
+    ];
     return Scaffold(
-      body: Center(
-        child: BookingCalendar(
-          bookingButtonColor: Theme.of(context).colorScheme.primary,
-          bookingGridCrossAxisCount:
-              MediaQuery.of(context).size.width >= 1024 ? 6 : 3,
-          bookingService: mockBookingService,
-          convertStreamResultToDateTimeRanges: convertStreamResultMock,
-          getBookingStream: getBookingStreamMock,
-          uploadBooking: uploadBookingMock,
-          startingDayOfWeek: StartingDayOfWeek.sunday,
-          pauseSlots: generatePauseSlots(),
-          pauseSlotText: 'Lunch',
-          hideBreakTime: false,
-          loadingWidget: const Text('Fetching data...'),
-          uploadingWidget: const CircularProgressIndicator(),
-          wholeDayIsBookedWidget:
-              const Text('Sorry, for this day everything is booked'),
-          //disabledDates: [DateTime(2023, 1, 20)],
-          //disabledDays: [6, 7],
-        ),
+      body: Column(
+        children: [
+          CalendarTimeline(
+            // showYears: true,
+            initialDate: DateTime.now(),
+            firstDate: DateTime.now().subtract(const Duration(days: 1)),
+            lastDate: DateTime.now().add(const Duration(days: 365)),
+            onDateSelected: (date) => print(date),
+            leftMargin: MediaQuery.of(context).size.width * 0.35,
+            monthColor: Colors.blueGrey,
+            dayColor: Theme.of(context).colorScheme.primary,
+            activeDayColor: Colors.white,
+            activeBackgroundDayColor: Theme.of(context).colorScheme.primary,
+            dotsColor: Color(0xFF333A47),
+            selectableDayPredicate: (date) {
+              return date
+                  .isAfter(DateTime.now().subtract(const Duration(days: 1)));
+            },
+            locale: 'en_ISO',
+          ),
+          SizedBox(height: 12),
+          Expanded(
+            child: GridView.builder(
+              padding: EdgeInsets.all(6),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount:
+                      MediaQuery.of(context).size.width >= 768 ? 5 : 3),
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  // color: (index % 2 == 0) ? Colors.green : Colors.yellow,
+                  child: Center(
+                    child: OutlinedButton(
+                      onPressed: () {},
+                      child: Text(items[index]),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          // Expanded(
+          //   child: Row(
+          //     children: [
+          //       Spacer(flex: 2),
+          //       Expanded(
+          //         flex: MediaQuery.of(context).size.width >= 1024 ? 1 : 6,
+          //         child: ListView.builder(
+          //           itemCount: items.length,
+          //           itemBuilder: (BuildContext context, int index) {
+          //             return ListTile(
+          //               title: OutlinedButton(
+          //                 style: OutlinedButton.styleFrom(elevation: 0),
+          //                 onPressed: () {},
+          //                 child: Text(items[index]),
+          //               ),
+          //             );
+          //           },
+          //         ),
+          //       ),
+          //       Spacer(flex: 2),
+          //     ],
+          //   ),
+          // ),
+          Padding(
+            padding: EdgeInsets.all(6),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 6),
+                    child: OutlinedButton(
+                      onPressed: () {
+                        Modular.to.navigate('/dash/appointment/booking');
+                      },
+                      style: OutlinedButton.styleFrom(
+                        foregroundColor: Colors.red,
+                        backgroundColor: Colors.red.shade300.withAlpha(10),
+                      ),
+                      child: const Text(
+                        'Cancel',
+                        style: TextStyle(color: Colors.red),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 6),
+                    child: OutlinedButton(
+                      onPressed: () {},
+                      child: Text('Book'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
